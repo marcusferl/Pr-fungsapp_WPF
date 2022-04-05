14d7cc990ae0ae8000078351c567482e8cda51f7
marcusferl@weifer.de <marcus>
2022-04-05 13:25:16 +0200

    05.04.2022 13:24

--- Test_App/Views/Info.xaml.cs
+++ Test_App/Views/Info.xaml.cs
@@ -24,5 +24,10 @@ namespace Test_App.Views
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
